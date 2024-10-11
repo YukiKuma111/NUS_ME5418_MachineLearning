ecbef2f8644b505b979f6bb05f70dee798830d51
“YukiKuma111” <1343879876@qq.com>
2024-10-11 23:17:51 +0800

    [ziyue_build_env] modified reset, and merge zewen's ggym/ggym.py

--- ggym/ggym.py
+++ ggym/ggym.py
@@ -194,7 +194,7 @@ class Group24env(gym.Env,EzPickle):
             # Main engine: -1..0 off, 0..+1 throttle from 50% to 100% power. Engine can't work with less than 50% power.
             # Left-right:  -1.0..-0.5 fire left engine, +0.5..+1.0 fire right engine, -0.5..0.5 off
             np.array([-1, -1, -1]).astype(np.float32),
-            np.array([1, -1, 1]).astype(np.float32),
+            np.array([1, 1, 1]).astype(np.float32),
         )
         # observation space
         # we have a hull(main body), two wheels, three engines, ground contact
